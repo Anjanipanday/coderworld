5689014eb4245d681e6c54dd309e744409381b1b
Anjani Panday <98972015+Anjanipanday@users.noreply.github.com>
2022-02-11 23:33:47 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,114 @@
+<html>
+<head>
+    <link href="css/bootstrap.min.css" rel="stylesheet"/>
+    <link href="css/font-awesome.min.css" rel="stylesheet"/>
+	<link href="css/hover.css" rel="stylesheet" />
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+	<style>
+    body {
+        overflow-x: hidden;
+    }
+</style>
+</head>
+<body>
+<div class=" row">
+		<div class="col-sm-12" style="min-height:40px;background:black;">
+		<a style="margin-left:30px;"><span class="fa fa-phone" style="color:white;line-height:2.3;font-size:17px">&nbsp;&nbsp;&nbsp;91-9336541604</span></a>
+		<a style="margin-left:30px;"><span class="fa fa-envelope" style="color:white;line-height:2.3;font-size:17px">&nbsp;&nbsp;&nbsp;coderworld@gmail.com</span></a>
+		<a style="margin-left:850px;"><span class="fa fa-instagram" style="color:white;line-height:2.3;font-size:17px"></span></a>
+		<a style="margin-left:30px;"><span class="fa fa-facebook" style="color:white;line-height:2.3;font-size:17px"></span></a>
+		<a style="margin-left:30px;"><span class="fa fa-twitter" style="color:white;line-height:2.3;font-size:17px"></span></a>
+		<a style="margin-left:30px;"><span class="fa fa-linkedin" style="color:white;line-height:2.3;font-size:17px"></span></a>
+		</div>
+		</div>
+<div class=" row" style="border-bottom:1px solid black;">
+                <div class="col-sm-2" style=" min-height:80px;">
+                    <div class="row">
+                        <img src="img/logo.png" style="height:70px;width:100%;margin-left:20px;margin-top:10px"/></div>
+                    </div>
+                <div class="col-sm-10" style="min-height:80px;">
+                  <nav class="navbar navbar-default"style="min-height:60px;color:black;width:100%;border:none;background:none;margin-top:10px">
+                <div class="container-fluid">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                     </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+                            <li><a href="index.php"style="font-size: 25px;line-height:40px;font-weight: bold; color:black;font-family: Gabriola">Home</a></li>
+                            <li><a href="aboutus.php" style="font-size: 25px;line-height:40px; font-weight: bold; color:black; font-family: Gabriola">About us</a></li>
+                            <li><a href="became.php" style="font-size: 25px; line-height:40px;font-weight: bold; color:black;font-family: Gabriola">Become Member</a></li>
+                            <li><a href="course.php" style="font-size: 25px;line-height:40px; font-weight: bold; color:black; font-family: Gabriola">Courses</a></li>
+                            <li><a href="contact.php" style="font-size: 25px;line-height:40px; font-weight: bold; color:black; font-family: Gabriola">Contactus</a></li>
+                            <li><a href="review.php" style="font-size: 25px;line-height:40px; font-weight: bold; color:black; font-family: Gabriola">Review</a></li>
+                            <li><a href="practices.php" style="font-size: 25px;line-height:40px; font-weight: bold; color:black; font-family: Gabriola">Practice</a></li>
+                            <li><a href="login.php" style="font-size: 25px;line-height:40px; font-weight: bold; color:black; font-family: Gabriola">Login</a></li>
+                       </ul>
+                    </div><!-- /.navbar-collapse -->
+                </div><!-- /.container-fluid -->
+            </nav>
+             </div>
+          </div>
+ <div class="row">
+ <center><img src="img/abo1.jfif"  style="height:100px;width:260px;margin-top:40px"/></center>
+ <div class="col-sm-1"></div>
+ <div class="col-sm-5" style="margin-top:40px" >
+ <p style="font-size: 25px; color: black; font-family: Gabriola;text-align:justify">Our Coder world is a knowledge base blog. where you can learn ,find solutions on heigh level language like as:c#,c++ ,python ...etc
+you can find your problem solutions by discussion form where you can upload your problem and many user see your problem and they give solutions.
+By Became member you join my blog as a member.By Courses you can read hegh level language...java, php, python...etc
+you are also do practice in any highlevel language by practice menu.It Is provides a platform to
+ meet, learn and share their knowledge in the fields of Programming.It is to encourage students to learn about
+ competitive coding and enhance their problem-solving skills.</p>
+ </div>
+ <div class="col-sm-1"></div>
+ <div class="col-sm-4">
+ <center><img src="img/s2.png"  style="height:450px;width:450px;"/></center></div>
+ <div class="col-sm-1"></div>
+</div>           
+			
+<!---------------------------start footer-------------------->
+<div class="row" style="background:black;min-height: 300px;margin-top:60px; font-size: 23px; color: white; font-family: Gabriola;font-weight:bold">
+                <div class="col-sm-3" style="margin-left:30px">
+                  <h1>Information</h1><hr>
+				  <ul>
+				  <li>AboutUs</li>
+				  <li>Contactus</li>
+				  <li>Becomemember</li>
+				  <li>Review</li>
+				  <li>Courses</li>
+				  <li>Practice</li>
+                </ul></div>
+                <div class="col-sm-3">
+                    <h1> Contact us</h1>
+                    <hr>
+                    <span class="fa fa-phone"></span>&nbsp;&nbsp;&nbsp;+91-9336541604<br>
+                    <span class="fa fa-envelope"></span>&nbsp;&nbsp;&nbsp;coderworld@gmail.com<br>
+                    <span class="fa fa-fax"></span>&nbsp;&nbsp;&nbsp;MMIT Siddharthanagar (Bansi)<br>
+                    </div>
+					<div class="col-sm-2">
+					<h1>Follow Us:</h1><hr>          
+					<img src="img/facebook.png"style="height:35px;width:40px;float:left;margin-left:5px" /> 
+                        <img src="img/linkedin.png" style="height: 30px; width: 40px; float: left; margin-left: 5px" />
+                        <img src="img/twitter.png" style="height: 30px; width: 40px; float: left; margin-left: 5px" />
+                        <img src="img/instagram.png" style="height: 35px; width: 40px; float: left; margin-left: 5px"/>
+                     </div>
+                <div class="col-sm-3">
+                  <h1>Categories</h1><hr>
+                <button style="background:black;padding-left:5px">ANGULAR</button> <button style="background:black;padding-left:5px">ASP.NET</button> <button style="background:black;padding-left:5px">C#</button> <button style="background:black;padding-left:5px">DOT NET</button> <button style="background:black;padding-left:5px">GENERAL</button> <button style="background:black;padding-left:5px">INTERVIEW QUESTIONS</button> <button style="background:black;padding-left:5px">JAVA</button>
+ <button style="background:black;padding-left:5px" >JAVA 8</button><button style="background:black;padding-left:5px">PYTHON</button><button style="background:black;padding-left:5px">SQL SERVER</button> <button style="background:black;padding-left:5px">TIPS/TRICS</button> <button style="background:black;padding-left:5px">WEB AP</button>
+                    
+              </div>
+            </div>
+<div class="row">
+<div class="col-sm-12" style="background:maroon"><p style="color:white;font-family:Gabriola;font-size:20px;text-align:center">Copyright&copy; MMIT Siddharthanagar & Devloped by A2RP</p></div>
+</div>
+</body>
+</html>
